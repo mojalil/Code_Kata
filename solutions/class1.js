1cad8394c22d47ea092dd0946d52085040e54b42
mjdude <mo@syncspot.net>
2017-01-08 10:42:53 +0000

    solutions to class1

---
+++ solutions/class1.js
@@ -0,0 +1,23 @@
+// Q1. Using the terminal list all the folders
+// A1. $ls
+// Q2. Use only the terminal to create a new folder called 'MasterFolder'
+// A2. $mkdir MasterFolder
+// Q3. Using the terminal go inside MasterFolder and create 'ChildFolder'
+// A3.
+//   $cd MasterFolder
+//   $mkdir ChildFolder
+// Q4. Create a Javascript file and print out what your current folder and directory is (hint, you can use the terminal to find out the current folder you are in)
+// A4.
+// $pwd
+// /home/mo/github/Code_Kata
+// var path = '/home/mo/github/Code_Kata';
+// console.log(path);
+
+// A4(alternative).
+// var path = require('path');
+// console.log(path.dirname(__dirname));
+
+// Q5. In the javascript file create an array with the numbers 1 to 10. Print out all the even numbers.
+// A5.
+// var numbers = [1,2,3,4,5,6,7,8,9,10];
+// console.log(numbers.filter( num =>  num % 2 ? false : true ));
