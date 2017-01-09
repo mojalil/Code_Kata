6b58c5b86d6309b9c629209a7ae02f27446c28d7
mjdude <mo@syncspot.net>
2017-01-08 22:23:26 -0500

    added code from live class

---
+++ class_code/class1.js
@@ -0,0 +1,21 @@
+// var name = 'Mo';
+// var age = 31;
+//
+// // this is an array , defined with square brackets
+// var array = [name, age];
+//
+//
+// console.log(array[0]);
+//
+// "My name is Mo and i am 31"
+
+// var dir = "/c/Users/Mo";
+// console.log(dir);
+
+// var path = require('path');
+// console.log(path.dirname(__dirname));
+// 
+// var numbers = [1,2,3,4,5,6,7,8,9,10];
+// console.log(numbers.filter( num =>  num % 2 ? false : true ));
+//
+// console.log(numbers[1], numbers[3], numbers[5], numbers[7], numbers[9]);
---
+++ class_code/class2.js
@@ -0,0 +1,60 @@
+// var boolTrue = true;
+// var boolFalse = false;
+// var isTrue = 10 > 9;
+// // is the same as
+// var setTrue = true;
+
+// console.log('isTrue is', isTrue);
+// console.log('setTrue is', setTrue);
+
+// // comparitors
+// > grater than
+// < less than
+// === is equal too
+// != does not equal
+//
+// console.log('10 != 9 should return true: answer is ', 10 != 9);
+
+
+// this is en object, use curly braces
+var person = {
+  name: 'Mo',
+  age: 31,
+  height: 173,
+  location: [],
+};
+
+var person2 = {
+  name: 'jo',
+  age: 31,
+  height: 173,
+  location: [],
+};
+
+
+// Another great feature isthat you can overwrite values. change persons name !
+// person.name = 'Jo';
+//
+// console.log('the persons name is', person.name);
+//
+// function printName(name){
+//   return 'name is ' + name;
+// };
+// console.log('hello');
+function add( number1, number2) {
+  return number1 + number2;
+}
+
+function subtract( num1 , num2){
+  return num1 - num2
+}
+
+console.log(subtract(5,4));
+console.log(add(1,2));
+console.log(add(3,4));
+
+
+// 1. CREATE a function that returns a name !
+// 2. CALL that function and pass in the name of the MOVIE !
+// 3. CONSOLE.LOG it out
+
