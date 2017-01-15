5aa00532aee5210582b3b878945dbf2b9e3cfe89
mjdude <mo@syncspot.net>
2017-01-14 22:49:51 -0500

    updated solution to class 2

--- solutions/class2.js
+++ solutions/class2.js
@@ -1,19 +1,30 @@
+// Q1. Create the object Spaceship. It should have colour, top speed , number of seats, which planets it's visited.
+// A1.
+
+var Spaceship = {
+  colour: "crimson",
+  topSpeed: 299792458,
+  numberOfSeets: 4,
+  planetVisited: ['Methuselah', 'Gliese', 'Gaia' ]
+}
+console.log(Spaceship.topSpeed)
+
 // Q3. Create a bucketlist object. Things you want to do before DYING. With 3 functions (list, add and remove). List should list all items. Add should add items and remove should remove items from the list.
 // A3. In progress
-var bucketList = {
-  list : ['learn to code'],
-  printList : function() {
-    return this.list;
-  },
-  add : function(item){
-    return this.list.push(item);
-  },
-  remove: function(){
-    return this.list.pop(item);
-  }
-}
+// var bucketList = {
+//   list : ['learn to code'],
+//   printList : function() {
+//     return this.list;
+//   },
+//   add : function(item){
+//     return this.list.push(item);
+//   },
+//   remove: function(){
+//     return this.list.pop(item);
+//   }
+// }
 
-// console.log(bucketList.list);
-console.log(bucketList.printList());
-console.log(bucketList.add('be awesome'));
-console.log(bucketList.printList());
+// // console.log(bucketList.list);
+// console.log(bucketList.printList());
+// console.log(bucketList.add('be awesome'));
+// console.log(bucketList.printList());
